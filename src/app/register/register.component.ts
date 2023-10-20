d9b52a30965b1e7a89f5b921728c6282c7e9b680
23po <ckodalo@gmail.com>
2023-10-20 18:42:46 +0300

    auth flow

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+   form: any = {
+    username: null,
+    email: null,
+    password:null
+   };
 
+   isSuccessful = false;
+   isRegisteredFailed = false;
+   errorMessage = '';
+
+   constructor(private authService: AuthService) {}
+
+   ngOnInit(): void {
+
+   }
+
+   onSubmit(): void {
+    const {username, email, password} = this.form;
+
+    this.authService.register(username, email, password).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isRegisteredFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isRegisteredFailed = true;
+      }
+    )
+   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password:string): Observable<any> {
-    return this.http.post(AUTH_API + 'login', {
+    return this.http.post(AUTH_API + 'authenticate', {
       username,
       password
     }, httpOptions);
