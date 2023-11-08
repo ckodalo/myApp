7af432e443ffc73b98946a1c9c0b4fba12dcac53
23po <ckodalo@gmail.com>
2023-11-08 09:04:19 +0300

    adds admin route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [  { 
   path: 'login', component: LoginComponent },
 { path: 'register', component: RegisterComponent },
 { path: 'profile', component: ProfileComponent},
-{ path: 'home', component: HomeComponent} 
+{ path: 'home', component: HomeComponent} ,
+{path: 'admin', component: AdminComponent}
 ];
 
 @NgModule({
