53aab9b5b9d8cd37068ab967cd602bb582b5b01e
23po <ckodalo@gmail.com>
2023-12-15 16:05:00 +0300

    carousel

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss']
+})
+export class CarouselComponent {
+
+  array = [1, 2, 3, 4];
+  dotPosition = 'bottom';
+
+}
