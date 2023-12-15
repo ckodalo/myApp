46ae1f7450c7e89adb441252fa40a42f76f58aaf
23po <ckodalo@gmail.com>
2023-12-15 16:07:30 +0300

    edit routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [  {
   path: 'login', component: LoginComponent },
 { path: 'register', component: RegisterComponent },
 { path: 'profile', component: ProfileComponent},
-{ path: 'home', component: HomeComponent} ,
+{ path: '', component: HomeComponent} ,
 {path: 'admin', component: AdminComponent}
 ];
 
